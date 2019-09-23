1e662cad35e069961d0d4201bd2b7c3172847a26
Nina Louw <nina@allay.io>
2019-09-23 14:10:22 -0700

    add headless

--- reservations.py
+++ reservations.py
@@ -90,7 +90,7 @@ class ReservationFinder(object):
     chrome_options.binary_location = GOOGLE_CHROME_BIN if GOOGLE_CHROME_BIN else {}
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--remote-debugging-port=9222')
 
